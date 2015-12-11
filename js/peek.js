e5fa50b2267d9c62c56736db648d64f60fa4192e
Taylor Rees <hello@taylorrees.co>
2015-12-11 12:07:17 +0000

    Removed peek buttons. Commented the Hash mini library.

--- js/peek.js
+++ js/peek.js
@@ -96,73 +96,66 @@ var Peek = (function() {
     }
   }
 
-  function getNavigationButtons() {
-    // Get the navigation buttons from the DOM
-    var peakControls = document.querySelector('.peek-controls');
-    return {
-      previous: peakControls.children[0],
-      toggle: peakControls.children[1],
-      next: peakControls.children[2]
-    };
-  }
-
-  function bindNavigationButtons(previous, toggle, next) {
-    // Binds the click event for navigation button click.
-    previous.onclick = function() { Peek.previous() };
-    toggle.onclick = function() { Peek.toggle() };
-    next.onclick = function() { Peek.next() };
-  }
-
-  function toggleControls() {
-    // Shows peek navigation buttons when drawer is open.
-    var navigationButtons = getNavigationButtons();
-    // Check when drawer is closed
-    if (isPeekActive() == false) {
-      // On click, when drawer closed -> draw will open -> show controls
-      navigationButtons.previous.style.display = 'inline';
-      navigationButtons.next.style.display = 'inline';
-    } else {
-      // On click, when drawer open -> draw will close -> hide controls
-      navigationButtons.previous.style.display = 'none';
-      navigationButtons.next.style.display = 'none';
-    }
-  }
-
   // Small library to get, add and remove hash values to / from url.
   var Hash = {
 
     get : function() {
-      // Gets the URL parameters and returns their value.
+      /**
+       * Gets the URL parameters and returns their value.
+       *
+       * @return {object}
+       */
       var result = {};
       var hash = window.location.hash.split('#');
-      for (var i = 0; i < hash.length; i++) {
-        var query = hash[i].split('=');
+      var query;
+      var i;
+
+      for (i = 0; i < hash.length; i++) {
+        query = hash[i].split('=');
         // parameter = value
         if (query[0] != '') result[query[0]] = query[1] != undefined ? query[1] : true;
       }
-      // returns object of hashes and their values
+      
       return result;
     },
 
-    add : function(parameter, value) {
-      // Adds to hash in the form #parameter=value (value is optional)
-      var hash = Hash.get();
+    set : function(hash) {
+      /**
+       * Writes the key, value pairs as hash.
+       * e.g. #key=value
+       *
+       * @param {object} hash
+       */
       var newHash = '';
-      hash[parameter] = value ? value : true;
+
       for (query in hash) {
         newHash = newHash + '#' + query + '=' + hash[query];
       }
       window.location.hash = newHash;
     },
 
+    add : function(parameter, value) {
+      /**
+       * Add parameter, value (optional) to existing hash.
+       *
+       * @param {string} parameter
+       * @param {string} value
+       */
+      var hash = this.get();
+      hash[parameter] = value ? value : true;
+      this.set(hash);
+    },
+
     remove : function(parameter) {
-      var hash = Hash.get();
-      var newHash = '';
+      /**
+       * Removes parameter from existing hash.
+       *
+       * @param {string} parameter
+       * @param {string} value
+       */
+      var hash = this.get();
       delete hash[parameter];
-      for (query in hash) {
-        newHash = newHash + '#' + query + '=' + hash[query];
-      }
-      window.location.hash = newHash;
+      this.set(hash);
     }
 
   };
@@ -175,40 +168,16 @@ var Peek = (function() {
     var peek = document.querySelector('.peek');
     var progress = document.createElement('div');
     var progressBar = document.createElement('span');
+
     progress.className = 'progress';
     progress.appendChild(progressBar);
     peek.appendChild(progress);
-  }
-
-  function injectControls() {
-    // Add the peek navigation bar to DOM.
-    // Injects: <div class="peek-controls"><a>&larr;</a><a>Peek</a><a>&rarr;</a></div>
-    var peekControls = document.createElement('div');
-    peekControls.className = 'peek-controls';
-    // previous
-    var previousButton = document.createElement('a');
-    previousButton.innerHTML = '&larr;';
-    // toggle
-    var toggleButton = document.createElement('a');
-    toggleButton.innerHTML = 'Peek';
-    // next
-    var nextButton = document.createElement('a');
-    nextButton.innerHTML = '&rarr;';
-
-    peekControls.appendChild(previousButton);
-    peekControls.appendChild(toggleButton);
-    peekControls.appendChild(nextButton);
-
-    // add click events
-    bindNavigationButtons(previousButton, toggleButton, nextButton);
-    document.body.appendChild(peekControls);
   }
 
   // Implementation
 
   function toggle() {
     // Toggles the state of the peek drawer (open / closed)
-    toggleControls();
     var peek = document.querySelector('.peek');
     if (peek.className == 'peek') {
       appendClass(peek, 'visible');
@@ -251,7 +220,6 @@ var Peek = (function() {
     }
     indexSlides(slides);
     setSlideLocations(slides);
-    injectControls();
     injectProgressBar();
     showProgress(slides);
     bindClickableKeys();
