b78d4fa8512480475750cd23a9c3d77f7184d5cb
Taylor Rees <hello@taylorrees.co>
2015-12-13 08:37:42 +0000

    Restructured the codebase. Added clear comments.

--- js/peek.js
+++ js/peek.js
@@ -1,105 +1,34 @@
 var Peek = (function() {
 
-  // Library
+  /**
+   * A set of required utilities for DOM and BOM manipulation.
+   *
+   */
+  var utils = {
 
-  function appendClass(s, c) {
-    // Accepts two strings: s and c.
-    // The function appends the class 'c' to the source 's'
+    appendClass : function(destination, classname) {
+      /**
+       * The function appends the class to the destination.
+       *
+       * @param {string} destination
+       * @param {string} classname
+       */
 
-    return s.className += ' ' + c;
-  }
+      return destination.className += ' ' + classname;
+    },
 
-  function removeClass(s, c) {
-    // Accepts two strings: s and c.
-    // The function removes the class 'c' from the source 's'
+    removeClass : function(source, classname) {
+      /**
+       * The function removes the class from the destination.
+       *
+       * @param {string} source
+       * @param {string} classname
+       */
 
-    return s.className = s.className.replace(c, '').trim();
-  }
+      return source.className = source.className.replace(classname, '').trim();
+    },
 
-  function indexSlides(slides) {
-    // Add indexes to slides
-    for (var i = 0; i < slides.count; i++) {
-      var slide = slides.list[i];
-      slide.dataset.index = i + 1;
-    }
-  }
-
-  function incrementSlides(slides) {
-    // Add one to slide order, largest value is n slides + 1
-    if (slides.next < slides.count + 1) {
-      slides.previous += 1;
-      slides.current += 1;
-      slides.next += 1;
-    }
-  }
-
-  function decrementSlides(slides) {
-    // Remove one from slide order, smallest value is 0
-    if (slides.previous > 0) {
-      slides.previous -= 1;
-      slides.current -= 1;
-      slides.next -= 1;
-    }
-  }
-
-  function setSlideLocation(slide, location) {
-    // Accepts two strings: slide and location.
-    return slide.dataset.location = location;
-  }
-
-  function setSlideLocations(slides) {
-    // Position slides within the peek drawer.
-    for (var i = 0; i < slides.count; i++) {
-      var slide = slides.list[i];
-      if (slide.dataset.index < slides.current) setSlideLocation(slide, 'left');
-      else if (slide.dataset.index > slides.current) setSlideLocation(slide, 'right');
-      else setSlideLocation(slide, 'current');
-    }
-  }
-
-  function showProgress(slides) {
-    // Sets the width of the progress bar.
-    var progress = document.querySelector('.peek .progress span');
-    var percentage = ((slides.current - 1) / (slides.count- 1) ) * 100;
-    progress.style.width = percentage + '%';
-  }
-
-  function isPeekActive() {
-    // Checks whether the peek drawer is open.
-    var peek = document.querySelector('.peek');
-    if (peek.className.indexOf('visible') != -1) return true;
-    return false;
-  }
-
-  function bindClickableKeys() {
-    // Binds clickable keys with actions.
-    document.onkeydown = function(e) {
-      if (e.ctrlKey && e.shiftKey && e.keyCode == '191') Peek.toggle(); // Ctrl + ?
-      if (isPeekActive() == true) {
-        if (e.keyCode == '40' || e.keyCode == '27') Peek.toggle(); // down arrow or Esc
-        if (e.keyCode == '37') Peek.previous(); // left arrow
-        if (e.keyCode == '39' || e.keyCode == '32') Peek.next(); // right arrow
-      }
-    };
-  }
-
-  function setSlideBackground(slide, url) {
-    // Sets the background image of the slide.
-    if (url) slide.style.backgroundImage = "url('" + url + "')";
-  }
-
-  function setBackgrounds(slides) {
-    // Sets the background image of slides with data-background attribute.
-    for (var i = 0; i < slides.count; i++) {
-      var slide = slides.list[i];
-      setSlideBackground(slide, slide.dataset.background);
-    }
-  }
-
-  // Small library to get, add and remove hash values to / from url.
-  var Hash = {
-
-    get : function() {
+    getHash : function() {
       /**
        * Gets the URL parameters and returns their value.
        *
@@ -113,13 +42,15 @@ var Peek = (function() {
       for (i = 0; i < hash.length; i++) {
         query = hash[i].split('=');
         // parameter = value
-        if (query[0] != '') result[query[0]] = query[1] != undefined ? query[1] : true;
+        if (query[0] != '') {
+          result[query[0]] = query[1] != undefined ? query[1] : true;
+        }
       }
-      
+
       return result;
     },
 
-    set : function(hash) {
+    setHash : function(hash) {
       /**
        * Writes the key, value pairs as hash.
        * e.g. #key=value
@@ -134,114 +65,289 @@ var Peek = (function() {
       window.location.hash = newHash;
     },
 
-    add : function(parameter, value) {
+    addHash : function(parameter, value) {
       /**
        * Add parameter, value (optional) to existing hash.
        *
        * @param {string} parameter
        * @param {string} value
        */
-      var hash = this.get();
+      var hash = this.getHash();
       hash[parameter] = value ? value : true;
-      this.set(hash);
+      this.setHash(hash);
     },
 
-    remove : function(parameter) {
+    removeHash : function(parameter) {
       /**
        * Removes parameter from existing hash.
        *
        * @param {string} parameter
        * @param {string} value
        */
-      var hash = this.get();
+      var hash = this.getHash();
       delete hash[parameter];
-      this.set(hash);
+      this.setHash(hash);
     }
 
   };
 
-  // DOM Injection
 
-  function injectProgressBar() {
-    // Add the progress bar to peek drawer.
-    // Injects: <div class="progress"><span></span></div>
-    var peek = document.querySelector('.peek');
-    var progress = document.createElement('div');
-    var progressBar = document.createElement('span');
+  var Slides = {
 
-    progress.className = 'progress';
-    progress.appendChild(progressBar);
-    peek.appendChild(progress);
-  }
+    allSlides : document.querySelector('.peek .slides').children,
+    previousSlideIndex : 0,
+    currentSlideIndex  : 1,
+    nextSlideIndex     : 2,
+
+    increment : function () {
+      /**
+       * Add one to slide order, largest value is n slides + 1.
+       *
+       */
+      if (this.nextSlideIndex < this.allSlides.length + 1) {
+        this.previousSlideIndex  += 1;
+        this.currentSlideIndex   += 1;
+        this.nextSlideIndex      += 1;
+      }
+    },
+
+    decrement : function () {
+      /**
+       * Remove one from slide order, smallest value is 0.
+       *
+       */
+      if (this.previousSlideIndex > 0) {
+        this.previousSlideIndex  -= 1;
+        this.currentSlideIndex   -= 1;
+        this.nextSlideIndex      -= 1;
+      }
+    },
+
+    setIndexes : function () {
+      /**
+       * Add indexes to slides.
+       *
+       */
+      var i;
+
+      for (i = 0; i < this.allSlides.length; i++) {
+        this.allSlides[i].dataset.index = i + 1;
+      }
+    },
+
+    setLocations : function () {
+      /**
+       * Position all slides within the peek drawer.
+       *
+       */
+
+      var slides = this.allSlides;
+      var slide;
+      var i;
+      var setLocation = function(slide, location) {
+        /**
+         * Sets the location of a slide in the slideshow.
+         *
+         * @param {HTMLElement} slide
+         * @param {string} location (left, right, current)
+         */
+        return slide.dataset.location = location;
+      }
+
+      for (i = 0; i < slides.length; i++) {
+        slide = slides[i];
+
+        if (slide.dataset.index < this.currentSlideIndex) {
+          setLocation(slide, 'left');
+        }
+        else if (slide.dataset.index > this.currentSlideIndex) {
+          setLocation(slide, 'right');
+        }
+        else {
+          setLocation(slide, 'current');
+        }
+      }
+    },
+
+    setBackgrounds : function () {
+      /**
+       * Set the background image of slides with
+       * data-background attribute.
+       *
+       */
+      var slides = this.allSlides;
+      var slide;
+      var i;
+      var setBackground = function(slide, url) {
+        /**
+         * Sets the background image of the slide.
+         *
+         * @param {HTMLElement} slide
+         * @param {string} url
+         */
+        if (url) slide.style.backgroundImage = "url('" + url + "')";
+      }
+
+      for (i = 0; i < slides.length; i++) {
+        slide = slides[i];
+        setBackground(slide, slide.dataset.background);
+      }
+    },
+
+    progressBar : function() {
+      /**
+       * Creates the progress bar.
+       * Sets the percentage width of the progress bar.
+       *
+       */
+      var current = this.currentSlideIndex;
+      var len = this.allSlides.length;
+      var progressBar = document.querySelector('.peek .progress span');
+
+      if (!progressBar) {
+        progressBar = (function createProgressBar() {
+          /**
+           * Add the progress bar to peek drawer.
+           * Injects: <div class="progress"><span></span></div>
+           *
+           */
+          var peek = document.querySelector('.peek');
+          var progress = document.createElement('div');
+          var progressBar = document.createElement('span');
+
+          progress.className = 'progress';
+          progress.appendChild(progressBar);
+          peek.appendChild(progress);
+
+          return progressBar;
+        })();
+      }
+
+      progressBar.style.width = ( ( (current - 1) / (len - 1) ) * 100 ) + '%';
+    }
+
+  };
+
 
   // Implementation
 
-  function toggle() {
-    // Toggles the state of the peek drawer (open / closed)
-    var peek = document.querySelector('.peek');
-    if (peek.className == 'peek') {
-      appendClass(peek, 'visible');
-      Hash.add('peek');
-    } else {
-      removeClass(peek, 'visible');
-      Hash.remove('peek');
+  var Peek = {
+
+    isActive : function() {
+      /**
+       * Checks whether the peek drawer is open.
+       *
+       */
+      var peek = document.querySelector('.peek');
+
+      if (peek.className.indexOf('visible') != -1) {
+        return true;
+      }
+
+      return false;
+    },
+
+    toggle : function() {
+      /**
+       * Toggles the state of the peek drawer (open / closed).
+       *
+       */
+      var peek = document.querySelector('.peek');
+
+      if (peek.className == 'peek') {
+        utils.appendClass(peek, 'visible');
+        utils.addHash('peek');
+      } else {
+        utils.removeClass(peek, 'visible');
+        utils.removeHash('peek');
+      }
+    },
+
+    next : function() {
+      /**
+       * Increments Slide indexes and displays the next slide.
+       *
+       */
+      var slides = Slides;
+      slides.increment();
+      slides.setLocations();
+      slides.progressBar();
+      utils.addHash('pslide', slides.currentSlideIndex);
+    },
+
+    previous : function() {
+      /**
+       * Decrements slide indexes and displays the previous slide.
+       *
+       */
+      var slides = Slides;
+      slides.decrement();
+      slides.setLocations();
+      slides.progressBar();
+      utils.addHash('pslide', slides.currentSlideIndex);
+    },
+
+    bindKeys : function() {
+      /**
+       * Binds clickable keys with actions.
+       *
+       */
+      var peek = this;
+
+      document.onkeydown = function(e) {
+
+        // Combination click: Ctrl + ?
+        if (e.ctrlKey && e.shiftKey && e.keyCode == '191') {
+          peek.toggle();
+        }
+
+        if (peek.isActive()) {
+
+          // Down arrow or Esc key click.
+          if (e.keyCode == '40' || e.keyCode == '27') {
+            peek.toggle();
+          }
+
+          // Left arrow click.
+          if (e.keyCode == '37') {
+            peek.previous();
+          }
+
+          // Right arrow click.
+          if (e.keyCode == '39' || e.keyCode == '32') {
+            peek.next();
+          }
+        }
+      };
+    },
+
+    init : function() {
+      /**
+       * Starts Peek.
+       *
+       */
+      var urlHash = utils.getHash();
+      var slides = Slides;
+
+      if (urlHash.pslide) {
+        // `pslide` is used to keep hash unique
+        // set index from pslide hash
+        var pslide = parseInt(urlHash.pslide);
+        slides.previousSlideIndex = pslide - 1;
+        slides.currentSlideIndex  = pslide;
+        slides.nextSlideIndex     = pslide + 1;
+      }
+
+      slides.setIndexes();
+      slides.setLocations();
+      slides.setBackgrounds();
+      slides.progressBar();
+
+      this.bindKeys();
+      if (urlHash.peek === 'true') this.toggle();
     }
-  }
 
-  function next() {
-    // Moves to and displays the next slide.
-    var slides = this.slides;
-    incrementSlides(slides);
-    setSlideLocations(slides);
-    showProgress(slides);
-    Hash.add('pslide', slides.current);
-  }
-
-  function previous() {
-    // Moves to and displays the previous slide.
-    var slides = this.slides;
-    decrementSlides(slides);
-    setSlideLocations(slides);
-    showProgress(slides);
-    Hash.add('pslide', slides.current);
-  }
-
-  function init(options) {
-    // Starts Peek.
-    var urlHash = Hash.get();
-    var slides = this.slides;
-    // pslide used to keep hash unique
-    if (urlHash.pslide) {
-      // set index from pslide hash
-      var current = parseInt(urlHash.pslide);
-      slides.previous = current - 1;
-      slides.current = current;
-      slides.next = current + 1;
-    }
-    indexSlides(slides);
-    setSlideLocations(slides);
-    injectProgressBar();
-    showProgress(slides);
-    bindClickableKeys();
-    setBackgrounds(slides);
-    // if peek is param --> show peek
-    if (urlHash.peek == 'true') this.toggle();
-  }
-
-  var Peek = {};
-
-  Peek.slides = {};
-  Peek.slides.list = document.querySelector('.peek .slides').children;
-  Peek.slides.count     = Peek.slides.list.length;
-  Peek.slides.previous  = 0;
-  Peek.slides.current   = 1;
-  Peek.slides.next      = 2;
-  Peek.toggle           = toggle;
-  Peek.next             = next;
-  Peek.previous         = previous;
-  Peek.init             = init;
-
-  // Initilization
+  };
 
   return Peek;
 
